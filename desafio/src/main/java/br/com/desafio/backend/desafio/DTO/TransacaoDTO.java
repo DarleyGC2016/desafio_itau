990bf0fb9cbc8c69486a156577e33fc826956e8f
DarleyGC2016 <garciacardoso.darley@gmail.com>
2025-08-28 19:28:26 -0300

    Commit do post operação transação

---
+++ desafio/src/main/java/br/com/desafio/backend/desafio/DTO/TransacaoDTO.java
@@ -0,0 +1,32 @@
+package br.com.desafio.backend.desafio.DTO;
+
+import java.time.OffsetDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TransacaoDTO {
+    private Double valor;
+    
+    private OffsetDateTime dataHora;
+
+    public  boolean validaTempo() {
+        OffsetDateTime agora = OffsetDateTime.now();
+        return getDataHora().isBefore(agora) && getDataHora().getYear() == agora.getYear() && getDataHora().getMonth() == agora.getMonth() && getDataHora().getDayOfMonth() != agora.getDayOfMonth() || getDataHora().getYear() < agora.getYear() || getDataHora().getMonthValue() < agora.getMonthValue();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TransacaoDTO\n  ")
+        .append("valor: ")
+        .append(getValor())        
+        .append("\n  ")
+        .append("dataHora: ").append(dataHora)
+                .append("\n");
+        return super.toString();
+    }
+
+}
