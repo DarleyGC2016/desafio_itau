f8b6f6ffbdc46a1e3b81e6dcb21a634211747105
DarleyGC2016 <garciacardoso.darley@gmail.com>
2026-02-28 00:19:34 -0300

    cors adicionado

--- desafio/src/main/java/br/com/desafio/backend/desafio/controller/TransacaoRest.java
+++ desafio/src/main/java/br/com/desafio/backend/desafio/controller/TransacaoRest.java
@@ -15,11 +15,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/desafio")
 public class TransacaoRest implements Crud<TransacaoRequest, ResponseEntity<String>> {
